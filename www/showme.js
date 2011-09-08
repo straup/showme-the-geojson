a1d77ef994ffb1a47eea82daf9682f81f3f7a15b
straup <straup@gmail.com>
2011-09-08 03:29:12 -0700

    start working through the FileReader / load features nightmare

--- www/showme.js
+++ www/showme.js
@@ -5,7 +5,7 @@ var documents = {};
 var extents = {};
 var properties = {};
 
-function showme_init(uri){
+function sm_init(uri){
 
 	var svg = org.polymaps.svg("svg");
 	var parent = document.getElementById('map').appendChild(svg);
@@ -31,13 +31,13 @@ function showme_init(uri){
 	map.add(compass);
 
 	if (uri){
-		showme_loadjson_uri(uri);
+		sm_loadjson_uri(uri);
 	}
 }
 
-function showme_loadjson_uri(uri){
+function sm_loadjson_uri(uri){
 
-	toggle_form('close');
+	sm_toggle_form('close');
 
 	if (extents[uri]){
 		return;
@@ -46,17 +46,14 @@ function showme_loadjson_uri(uri){
 	var layer = org.polymaps.geoJson();
 	layer.url(uri);
 
-	var onload = showme_onload(uri);
-	layer.on('load', onload());
+	var onload = sm_onload(uri, 1);
+	layer.on('load', onload);
 
 	map.add(layer);
-
 	documents[uri] = layer;
 }
 
-// this doesn't work for reasons I don't understand...
-
-function showme_loadjson_features(features, uri){
+function sm_loadjson_features(features, uri){
 
 	if (! uri){
 		uri = hex_md5(JSON.stringify(features));
@@ -66,44 +63,36 @@ function showme_loadjson_features(features, uri){
 		return;
 	}
 
-	var js_tiles = org.polymaps.geoJson();
-	js_tiles.features(features);
+	var layer = org.polymaps.geoJson();
+	layer.features(features);
 
-	// specifically, whenever this is triggered it seems
+	// something is very very wrong here...
+
+	// specifically, whenever this is called with set_extent (1)
 	// to launch itself in to a spiral death march calling
 	// itself over and over and over again; this does not
 	// happen fetch remote URLs...
 
-	var onload = showme_onload(uri);
-	js_tiles.on('load', onload());
+	var onload = sm_onload(uri, 0);
+	layer.on('load', onload);
 
-	map.add(js_tiles);
+	map.add(layer);
+	documents[uri] = layer;
 }
 
-function showme_onload(uri){
+// TO DO: fuck this noise and calculate extent and
+// set extent in the formhandler not here
 
-	return function(){
+function sm_onload(uri, set_extent){
 
-		// only set the extent of the geojson features once...
-		var set_extent = 1;
+	return function(e){
 
-		return function(e){
-
-			showme_onloadjson(e, uri, set_extent);
-
-			if (set_extent){
-				showme_list_documents();
-			}
-
-			set_extent = 0;
-		}
+		sm_onloadjson(e, uri, set_extent);
+		set_extent = 0;
 	}
-
 }
 
-function showme_onloadjson(geojson, uid, set_extent){
-
-	// check for extents[uid] here...
+function sm_onloadjson(geojson, uid, set_extent){
 
 	if (! properties[uid]){
 		properties[uid] = new Array();
@@ -195,7 +184,6 @@ function showme_onloadjson(geojson, uid, set_extent){
 
 		var pid = uid + "#" + i;
 		var hex = hex_md5(pid);
-
 		var el = feature.element;
 
 		if (data.geometry.type == 'GeometryCollection'){
@@ -203,8 +191,8 @@ function showme_onloadjson(geojson, uid, set_extent){
 		}
 
 		else {
-			el.setAttribute('onmouseover', 'showme_show_properties("' + pid + '");');
-			el.setAttribute('onclick', 'showme_copy_to_clipboard("' + pid + '");');
+			el.setAttribute('onmouseover', 'sm_show_properties("' + pid + '");');
+			el.setAttribute('onclick', 'sm_copy_to_clipboard("' + pid + '");');
 
 			el.setAttribute('class', data.geometry.type.toLowerCase());
 			el.setAttribute('id', hex);
@@ -238,9 +226,7 @@ function showme_onloadjson(geojson, uid, set_extent){
 	}
 }
 
-function showme_list_documents(){
-
-	// fix me: rename me
+function sm_list_documents(){
 
 	var docs = document.getElementById("documents");
 	docs.innerHTML = '';
@@ -254,7 +240,7 @@ function showme_list_documents(){
 		var name = document.createElement('a');
 		var txt = document.createTextNode(uid);
 		name.appendChild(txt);
-		name.setAttribute('onclick', 'showme_jumpto("' + uid + '");');
+		name.setAttribute('onclick', 'sm_jumpto("' + uid + '");');
 
 		var del = document.createElement('a');
 		del.setAttribute("class", "sm_close");
@@ -267,7 +253,7 @@ function showme_list_documents(){
 		bbox = [ bbox[0].lat, bbox[0].lon, bbox[1].lat, bbox[1].lon ];
 
 		var extent = document.createElement('div');
-		extent.setAttribute('class', 'bbox');
+		extent.setAttribute('class', 'sm_bbox');
 		extent.appendChild(document.createTextNode(bbox.join(', ')));
 
 		doc.appendChild(name);
@@ -287,7 +273,7 @@ function showme_list_documents(){
 		var txt = document.createTextNode("show all");
 
 		link.appendChild(txt);
-		link.setAttribute('onclick', 'showme_jumpto();');
+		link.setAttribute('onclick', 'sm_jumpto();');
 		control.appendChild(link);
 		list.appendChild(control);
 	}
@@ -328,15 +314,25 @@ function remove_document(uid){
 		{ 'lat': nelat, 'lon': nelon },
 	];
 
-	showme_jumpto();
-	showme_list_documents();
+	sm_jumpto();
+	sm_list_documents();
 }
 
-function showme_jumpto(uid){
+function sm_jumpto(uid){
 
-	showme_hide_properties();
+	sm_hide_properties();
 
 	if ((! uid) || (! extents[uid])){
+
+		if (map_extent[0]['lat'] == null){
+			map.extent([
+				{ 'lat': -85, 'lon': -180 },
+				{ 'lat': 85, 'lon': 180 },
+			]);
+			map.zoom(1);
+			return;
+		}
+
 		map.extent(map_extent);
 		map.zoom(Math.floor(map.zoom()));
 		return;
@@ -346,9 +342,7 @@ function showme_jumpto(uid){
 	map.zoom(Math.floor(map.zoom()));
 }
 
-function showme_show_properties(pid){
-
-	// pid == document uri + '#' + feature idx
+function sm_show_properties(pid){
 
 	var active = document.getElementsByClassName('geom-active');
 	var count_active = active.length;
@@ -386,7 +380,7 @@ function showme_show_properties(pid){
 	header.appendChild(document.createTextNode(uid + ', item #' + (Number(idx) + 1)));
 	props.appendChild(header);
 
-	var list = showme_domify_properties(data);
+	var list = sm_domify_properties(data);
 
 	var control = document.createElement('li');
 	var link = document.createElement('a');
@@ -394,15 +388,17 @@ function showme_show_properties(pid){
 	var txt = document.createTextNode("hide these properties");
 
 	link.appendChild(txt);
-	link.setAttribute('onclick', 'showme_hide_properties();');
+	link.setAttribute('onclick', 'sm_hide_properties();');
 	control.appendChild(link);
-	list.appendChild(control);
+	list.insertBefore(control, list.firstChild);
+
+	// TO DO: if there are no children then auto-close on mouseout
 
 	props.appendChild(list);
 	props.style.display = 'block';
 }
 
-function showme_domify_properties(data){
+function sm_domify_properties(data){
 
 	var list = document.createElement('ul');
 	list.setAttribute('class', 'properties');
@@ -432,13 +428,13 @@ function showme_domify_properties(data){
 	return list;
 }
 
-function showme_hide_properties(){
+function sm_hide_properties(){
 	var props = document.getElementById("properties");
 	props.innerHTML = '';
 	props.style.display = 'none';
 }
 
-function showme_formhandler(){
+function sm_formhandler(){
 
 	var uri = document.getElementById("fetchuri");
 	var file = document.getElementById("fetchfile");
@@ -446,68 +442,66 @@ function showme_formhandler(){
 	if (uri.value){
 
 		if (uri.value.indexOf("http://") == 0){
-			showme_loadjson_uri(uri.value);
-
+			sm_loadjson_uri(uri.value);
 			uri.value = '';
-			return;
 		}
 
-		alert("Invalid URL!");
+		else {
+			alert("Invalid URL!");
+		}
 	}
 
 	if (file.files.length){
-		showme_loadfiles(file.files);
-
+		sm_loadfiles(file.files);
 		file.value = '';
-		return;
 	}
+
+	sm_list_documents();
+	sm_toggle_form('close');
 }
 
-function showme_loadfiles(files){
+function sm_loadfiles(files){
 
 	var count_files = files.length;
 
 	for (var i=0; i < count_files; i++){
-		showme_loadfile(files[i]);
+		sm_loadfile(files[i]);
 	}
 
 }
 
-function showme_loadfile(file){
+function sm_loadfile(file){
 
-	// see notes in showme_loadjson_features;
-	// this works in FF an Safari...
+	var file_reader;
 
 	try {
-		showme_loadjson_uri(file.name);
+		file_reader = new FileReader();
 	}
 
-	catch (e){
-		alert(e);
+	catch(e){
+		sm_loadjson_uri(file.name);
+		return;
 	}
 
-	// see notes in showme_loadjson_features
+	file_reader.onloadend = function(e){
+
+		try {
+			geojson = JSON.parse(e.target.result);
+		}
+
+		catch(e) {
+			alert('failed to parse your file (' + file.name + '), the error was: ' + e);
+			return;
+		}
+
+		sm_loadjson_features(geojson.features, file.name);
+	};
+
+	file_reader.readAsBinaryString(file);
 	return;
-
-	if (file.type != 'application/json'){
-		return;
-	}
-
-	var geojson = file.getAsBinary();
-
-	try {
-		geojson = JSON.parse(geojson);
-	}
-
-	catch(e) {
-		console.log(e);
-		return;
-	}
-
-	showme_loadjson_features(geojson.features, file.name);
 }
 
-function toggle_form(close){
+function sm_toggle_form(close){
 
 	var h = document.getElementById("load_header");
 	h.style.display = (close) ? 'block' : 'none';
@@ -516,7 +510,7 @@ function toggle_form(close){
 	f.style.display = (close) ? 'none' : 'block';
 }
 
-function showme_copy_to_clipboard(pid){
+function sm_copy_to_clipboard(pid){
 
 	var parts = pid.split("#");
 	var uid = parts[0];
@@ -527,7 +521,7 @@ function showme_copy_to_clipboard(pid){
 	}
 
 	var data = properties[uid][idx];
-	var list = showme_domify_properties(data);
+	var list = sm_domify_properties(data);
 
 	var clipboard = document.getElementById("clipboard");
 
@@ -543,11 +537,10 @@ function showme_copy_to_clipboard(pid){
 
 }
 
-function showme_close_clipboard(){
+function sm_close_clipboard(){
 	var clipboard = document.getElementById("clipboard");
 	var clipbody = document.getElementById("clipbody");
 	clipbody.innerHTML = '';
 	clipboard.style.display = 'none';
-
-	showme_hide_properties();
+	sm_hide_properties();
 }
